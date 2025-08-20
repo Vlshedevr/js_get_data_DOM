f51af5a84f5874eba79e227a73f7afac2be09ed1
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-08-20 13:21:52 +0300

    my first DOM task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const livePeople = document.querySelectorAll('.population');
+
+const value = [...livePeople].map(
+  (contryPopulation) => +contryPopulation.textContent.replace(/,/g, ''),
+);
+
+const allPeople = value.reduce((acum, oneContry) => acum + oneContry, 0);
+
+const middleAvarage = allPeople / value.length;
+
+const docAllPeaple = document.querySelector('.total-population');
+const docAveragePeaple = document.querySelector('.average-population');
+
+docAllPeaple.textContent = allPeople.toLocaleString('en-US');
+docAveragePeaple.textContent = middleAvarage.toLocaleString('en-US');
