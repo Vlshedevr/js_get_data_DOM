5d69c24a9b3dc0d9013a015c96d168417661691a
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-08-21 13:10:09 +0300

    try bag fix 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,29 @@
 
 const livePeople = document.querySelectorAll('.population');
 
-const value = [...livePeople].map(
-  (contryPopulation) => +contryPopulation.textContent.replace(/,/g, ''),
-);
+const arrayLivepeople = [...livePeople].map((number) => number.textContent);
 
-const allPeople = value.reduce((acum, oneContry) => acum + oneContry, 0);
+const parser = arrayLivepeople[0].match(/[^0-9]/)[0]; // мій роздільник
 
-const middleAvarage = allPeople / value.length;
+const strPeopl = arrayLivepeople.map((number) => number.replaceAll(parser, ''));
 
-const docAllPeaple = document.querySelector('.total-population');
-const docAveragePeaple = document.querySelector('.average-population');
+const numPepople = strPeopl
+  .map((strNum) => Number(strNum))
+  .filter((num) => !isNaN(num));
 
-docAllPeaple.textContent = allPeople.toLocaleString('en-US');
-docAveragePeaple.textContent = middleAvarage.toLocaleString('en-US');
+let totalPeople = numPepople.reduce((acum, number) => acum + number, 0);
+let averagePeople = totalPeople / numPepople.length;
+
+function formater(number, par) {
+  const num = Math.floor(number)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, par);
+
+  return num;
+}
+
+totalPeople = formater(totalPeople, parser);
+averagePeople = formater(averagePeople, parser);
+
+document.querySelector('.total-population').textContent = totalPeople;
+document.querySelector('.average-population').textContent = averagePeople;
